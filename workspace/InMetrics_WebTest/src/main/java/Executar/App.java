248533ccea93120358cf5caec41bae9dcfe02f7f
clopesbraga <clopesbraga@gmail.com>
2019-11-29 03:21:39 -0200

    Atualização para novos testes

---
+++ workspace/InMetrics_WebTest/src/main/java/Executar/App.java
@@ -0,0 +1,17 @@
+package Executar;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		glue= {"test"},
+		features = {"./src/test/java/features/Test.feature"}
+		)
+
+public class App 
+{
+ 
+}
